8e9834ccd3d6c47ce57aed6f66b73a54eac2f76f
PhongLee1402 <phongtodalee1402@gmail.com>
2022-12-30 05:37:40 +0700

    frist commit

---
+++ js/counter__number.js
@@ -0,0 +1,22 @@
+//#region - start of - number counter animation
+const counterAnim = (qSelector, start = 0, end, duration = 1000) => {
+    const target = document.querySelector(qSelector);
+    let startTimestamp = null;
+    const step = (timestamp) => {
+     if (!startTimestamp) startTimestamp = timestamp;
+     const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+     target.innerText = Math.floor(progress * (end - start) + start);
+     if (progress < 1) {
+      window.requestAnimationFrame(step);
+     }
+    };
+    window.requestAnimationFrame(step);
+   };
+   //#endregion - end of - number counter animation
+   
+   document.addEventListener("DOMContentLoaded", () => {
+    counterAnim(".count1", 0, 18, 1000);
+    counterAnim(".count2", 0, 20, 1000);
+    counterAnim(".count3", 0, 30, 1000);
+    counterAnim(".count4", 0, 50, 1000);
+   });
